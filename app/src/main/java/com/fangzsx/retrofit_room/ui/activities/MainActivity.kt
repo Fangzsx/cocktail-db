330463ea07f4cb343301c6ce3cd09dc112fa5d12
Jay Gilbert Garzon <jygrzn@gmail.com>
2022-04-15 22:40:24 +0800

    add confirmation dialog

--- app/src/main/java/com/fangzsx/retrofit_room/ui/activities/MainActivity.kt
+++ app/src/main/java/com/fangzsx/retrofit_room/ui/activities/MainActivity.kt
@@ -1,17 +1,44 @@
 package com.fangzsx.retrofit_room.ui.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Dialog
+import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.fangzsx.retrofit_room.R
 import com.fangzsx.retrofit_room.databinding.ActivityMainBinding
 import com.fangzsx.retrofit_room.ui.fragments.AboutFragment
 import com.fangzsx.retrofit_room.ui.fragments.FavoritesFragment
 import com.fangzsx.retrofit_room.ui.fragments.HomeFragment
 import com.fangzsx.retrofit_room.ui.fragments.SearchFragment
+import com.shashank.sony.fancydialoglib.Animation
+import com.shashank.sony.fancydialoglib.FancyAlertDialog
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+
+    override fun onBackPressed() {
+        FancyAlertDialog.Builder
+            .with(this)
+            .setTitle("Exit Application")
+            .setBackgroundColor(Color.parseColor("#D10D0D")) // for @ColorRes use setBackgroundColorRes(R.color.colorvalue)
+            .setMessage("Do you really wish to leave?")
+            .setNegativeBtnText("Cancel")
+            .setPositiveBtnBackgroundRes(R.color.red)
+            .setPositiveBtnText("Exit")
+            .setNegativeBtnBackgroundRes(R.color.blue)
+            .setAnimation(Animation.POP)
+            .isCancellable(true)
+            .onPositiveClicked { dialog: Dialog? ->
+                finish()
+            }
+            .onNegativeClicked { dialog: Dialog? ->
+
+            }
+            .build()
+            .show()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
