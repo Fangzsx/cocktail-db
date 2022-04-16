be29a25ce7121a724ba120fa98e41d2f143fd54f
Jay Gilbert Garzon <jygrzn@gmail.com>
2022-04-16 09:10:27 +0800

    change layout for exit dialog

--- app/src/main/java/com/fangzsx/retrofit_room/ui/activities/MainActivity.kt
+++ app/src/main/java/com/fangzsx/retrofit_room/ui/activities/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.fangzsx.retrofit_room.ui.activities
 
-import android.app.Dialog
-import android.graphics.Color
+import android.app.AlertDialog
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.fangzsx.retrofit_room.R
 import com.fangzsx.retrofit_room.databinding.ActivityMainBinding
@@ -11,34 +9,31 @@ import com.fangzsx.retrofit_room.ui.fragments.AboutFragment
 import com.fangzsx.retrofit_room.ui.fragments.FavoritesFragment
 import com.fangzsx.retrofit_room.ui.fragments.HomeFragment
 import com.fangzsx.retrofit_room.ui.fragments.SearchFragment
-import com.shashank.sony.fancydialoglib.Animation
-import com.shashank.sony.fancydialoglib.FancyAlertDialog
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
 
+
     override fun onBackPressed() {
-        FancyAlertDialog.Builder
-            .with(this)
-            .setTitle("Exit Application")
-            .setBackgroundColor(Color.parseColor("#D10D0D")) // for @ColorRes use setBackgroundColorRes(R.color.colorvalue)
-            .setMessage("Do you really wish to leave?")
-            .setNegativeBtnText("Cancel")
-            .setPositiveBtnBackgroundRes(R.color.red)
-            .setPositiveBtnText("Exit")
-            .setNegativeBtnBackgroundRes(R.color.blue)
-            .setAnimation(Animation.POP)
-            .isCancellable(true)
-            .onPositiveClicked { dialog: Dialog? ->
-                finish()
-            }
-            .onNegativeClicked { dialog: Dialog? ->
+        val customLayout = layoutInflater.inflate(R.layout.exit_dialog, null)
+
+        AlertDialog.Builder(this)
+            .setView(customLayout)
+
+            //change text colors
+            .setPositiveButton("YES"){ _,_ ->
 
             }
-            .build()
+            .setNegativeButton("CANCEL"){ _,_ ->
+
+            }
+            .create()
             .show()
+
+
     }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,8 +46,6 @@ class MainActivity : AppCompatActivity() {
         binding.btmBarNav.onTabSelected = {
             launchFragment(it.title)
         }
-
-
     }
 
     private fun launchFragment(title: String) {
